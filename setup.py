b4c55c2a78b2dac0ff91f32e6e73a41f90268804
Ed Summers <ehs@pobox.com>
2011-06-09 18:59:38 -0400

    upped version to v0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'microdata',
-    version = '0.1',
+    version = '0.2',
     description = "html5lib extension for parsing microdata",
     author = "Ed Summers",
     author_email = "ehs@pobox.com",
