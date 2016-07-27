22b8c6d0152a93e0a74531662fa5863126b45998
Ed Summers <ehs@pobox.com>
2016-07-27 14:40:38 -0400

    require html5lib 0.999999999 or more

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info >= (3,):
 
 setup(
     name = 'microdata',
-    version = '0.5.5',
+    version = '0.6.0',
     description = "html5lib extension for parsing microdata",
     author = "Ed Summers",
     author_email = "ehs@pobox.com",
@@ -15,6 +15,6 @@ setup(
     py_modules = ['microdata'],
     scripts = ['microdata.py'],
     test_suite = 'test',
-    install_requires = ['html5lib'],
+    install_requires = ['html5lib>=0.999999999'],
     **extra
 )
