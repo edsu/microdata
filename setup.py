fbbed2d7fd31109545780a758734d9aefb6413e1
Ed Summers <ehs@pobox.com>
2019-07-12 09:12:50 -0400

    new version again

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info >= (3,):
 
 setup(
     name = 'microdata',
-    version = '0.7.0',
+    version = '0.7.1',
     description = "html5lib extension for parsing microdata",
     author = "Ed Summers",
     author_email = "ehs@pobox.com",
